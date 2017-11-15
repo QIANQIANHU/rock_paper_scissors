122520ee5a4689ac438bfa4f83da32b6863de91f
Qianqian Hu and Shane Robinson <qianqian@>
2017-11-15 11:25:05 -0800

    add the last half of the code and specs.

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,24 @@
-class RPS < String
+class String
   def rock_paper_scissors(user)
     if (self == "rock") & (user == "scissor")
-    return "computer wins"
+      return "computer wins"
     end
     if (self == "rock") & (user == "paper")
-    return "user wins"
+      return "user wins"
     end
     if (self == "scissor") & (user == "rock")
-    return "user wins"
+      return "user wins"
     end
-
+    if (self == "scissor") & (user == "paper")
+      return "computer wins"
+    end
+    if (self == "paper") & (user == "rock")
+      return "computer wins"
+    end
+    if (self == "paper") & (user == "scissor")
+      return "user wins"
+    else
+      return "It is a tie."
+    end
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -13,5 +13,16 @@ describe('rock_paper_scissors') do
     it("returns user wins if scissor is the object and rock is the argument") do
     expect("scissor".rock_paper_scissors("rock")).to(eq("user wins"))
     end
-    
+    it("returns user wins if scissor is the object and paper is the argument") do
+    expect("scissor".rock_paper_scissors("paper")).to(eq("computer wins"))
+    end
+    it("returns user wins if paper is the object and rock is the argument") do
+    expect("paper".rock_paper_scissors("rock")).to(eq("computer wins"))
+    end
+    it("returns user wins if paper is the object and scissor is the argument") do
+    expect("paper".rock_paper_scissors("scissor")).to(eq("user wins"))
+    end
+    it("returns 'It is a tie.' if object and argument are the same") do
+    expect("paper".rock_paper_scissors("paper")).to(eq("It is a tie."))
+  end
 end
